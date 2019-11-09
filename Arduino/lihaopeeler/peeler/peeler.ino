8f2a051a2d9afddbf017f70ba43b540f58db3544
bobydeng <deng569@163.com>
2019-11-09 11:11:23 +0000

    povled

--- Arduino/lihaopeeler/peeler/peeler.ino
+++ Arduino/lihaopeeler/peeler/peeler.ino
@@ -1,39 +1,49 @@
 
 #include <Servo.h>
 
-const int valvePin = 6;
+const int loadValvePin = 6;
+const int unloadValvePin = 7;
 const int btnPin = A0;
 
-const int door_o_angle=45;
-const int door_c_angle=0;
-const int pipe_load_angle=90;
-const int pipe_unload_angle=146;
+const int door_o_angle=105;
+const int door_c_angle=48;
+const int pipe_load_angle=45;
+const int pipe_unload_angle=74;
 
 Servo pipe_servo;
 Servo door_servo;
 
 void setup() {
-  pinMode(valvePin, OUTPUT);
+  pinMode(loadValvePin, OUTPUT);
+  pinMode(unloadValvePin, OUTPUT);
   pinMode(btnPin, INPUT);
   pipe_servo.attach(8);
   door_servo.attach(9);
+  
+  door_servo.write(door_o_angle);
+  delay(500);
+  pipe_servo.write(pipe_load_angle);
 }
 
 void loop() {
   int btn = digitalRead(btnPin);
   if(btn) {
+    blow(loadValvePin);
+    delay(1000);
+    
     //pipe_servo.write(90);
     pipe_servo.write(pipe_unload_angle);
     delay(500);
+    
     door_servo.write(door_c_angle);
     delay(500);
     
-    blow();
+    blow(unloadValvePin);
   
     door_servo.write(door_o_angle);
     delay(500);
   
-    blow();
+    blow(unloadValvePin);
   
     pipe_servo.write(pipe_load_angle);
     delay(500);
@@ -41,9 +51,9 @@ void loop() {
 }
 
 
-void blow() {
-  digitalWrite(valvePin, HIGH);
-  delay(500);
-  digitalWrite(valvePin, LOW);
+void blow(int valve) {
+  digitalWrite(valve, HIGH);
+  delay(200);
+  digitalWrite(valve, LOW);
 }
 
